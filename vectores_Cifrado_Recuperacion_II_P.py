7f50e1bd54f2005dc1aad1071d8e7f73f7e2e824
alejandro <alejosamp@gmail.com>
2024-04-28 20:59:26 -0500

    documentation added

--- vectores_Cifrado_Recuperacion_II_P.py
+++ vectores_Cifrado_Recuperacion_II_P.py
@@ -1,6 +1,16 @@
-
 # Función para cifrar una palabra
 def cifrar_palabra(palabra, letras, cifrado):
+    """
+    Cifra una palabra utilizando un cifrado de sustitución.
+
+    Parámetros:
+    - palabra (str): La palabra a cifrar.
+    - letras (list): Lista de caracteres (letras y espacio) a cifrar.
+    - cifrado (list): Lista de caracteres que representan el cifrado de sustitución.
+
+    Retorna:
+    - str: La palabra cifrada.
+    """
     palabra_cifrada = ""
     for letra in palabra:
         if letra in letras:
@@ -8,11 +18,21 @@ def cifrar_palabra(palabra, letras, cifrado):
             palabra_cifrada += cifrado[indice]
         else:
             palabra_cifrada += letra
-
     return palabra_cifrada
 
 # Función para descifrar una palabra
-def decifrar_palabra(palabra_cifrada, letras, cifrado):
+def descifrar_palabra(palabra_cifrada, letras, cifrado):
+    """
+    Descifra una palabra cifrada utilizando un cifrado de sustitución.
+
+    Parámetros:
+    - palabra_cifrada (str): La palabra a descifrar.
+    - letras (list): Lista de caracteres (letras y espacio) a descifrar.
+    - cifrado (list): Lista de caracteres que representan el cifrado de sustitución.
+
+    Retorna:
+    - str: La palabra descifrada.
+    """
     palabra = ""
     for caracter in palabra_cifrada:
         if caracter in cifrado:
@@ -20,18 +40,22 @@ def decifrar_palabra(palabra_cifrada, letras, cifrado):
             palabra += letras[indice]
         else:
             palabra += caracter
-
     return palabra
 
 # Función principal del programa
 def main():
-    # Abecedario en mayúsculas y minúsculas, y espacio
+    """
+    Función principal del programa.
+
+    Muestra un menú que permite al usuario cifrar o descifrar una palabra utilizando un cifrado de sustitución.
+    """
+    # Alfabeto en mayúsculas y minúsculas, y espacio
     letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
     # Cifrado correspondiente
     cifrado = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "1", ":", ";", "12", "<", ">", ",", ".", "/", "3", "~", "`", "!", "¡", "¿", "§", "¶", "4", "ª", "º", "°", "¤", "¢", "£", "¥", "5", "©", "®", "6", "µ", "7", "8", "9", "0"]
 
     while True:
-        print("\nMENU:")
+        print("\nMENÚ:")
         print("1. Cifrar palabra")
         print("2. Descifrar palabra")
         print("3. Salir")
@@ -39,12 +63,12 @@ def main():
         opcion = input("Seleccione una opción: ")
 
         if opcion == '1':
-            palabra_original = input("Digite una palabra para cifrar: ")
+            palabra_original = input("Ingrese una palabra para cifrar: ")
             palabra_cifrada = cifrar_palabra(palabra_original, letras, cifrado)
             print("Palabra cifrada:", palabra_cifrada)
         elif opcion == '2':
-            palabra_cifrada = input("Digite una palabra cifrada para descifrar: ")
-            palabra_descifrada = decifrar_palabra(palabra_cifrada, letras, cifrado)
+            palabra_cifrada = input("Ingrese una palabra cifrada para descifrar: ")
+            palabra_descifrada = descifrar_palabra(palabra_cifrada, letras, cifrado)
             print("Palabra descifrada:", palabra_descifrada)
         elif opcion == '3':
             print("¡Hasta luego!")
